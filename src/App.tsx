9e2459acfa8fae1e45055972e9b949a9096b4035
Feetu Nyrhinen <feetu.nyrhinen@gmail.com>
2020-02-25 16:55:47 +0100

    Adds sample data and a sample visualization by using the library Victory

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Container from '@material-ui/core/Container'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -8,14 +8,30 @@ import SettingsContent from './pages/SettingsContent'
 import {HOME, SETTINGS, VISUALIZATIONS} from './general/constants'
 
 const App = () => {
+  const sampleMeas = [
+    {'measurementDate': '1582125306', 'temperature': 22.1, 'humidity': 65.0},
+    {'measurementDate': '1582211706', 'temperature': 23.2, 'humidity': 68.4},
+    {'measurementDate': '1582298106', 'temperature': 20.9, 'humidity': 75.0},
+    {'measurementDate': '1582384506', 'temperature': 21.3, 'humidity': 78.2},
+    {'measurementDate': '1582470906', 'temperature': 22.0, 'humidity': 66.1},
+    {'measurementDate': '1582557306', 'temperature': 22.6, 'humidity': 67.2},
+    {'measurementDate': '1582643706', 'temperature': 21.7, 'humidity': 68.3},
+  ]
   const [activePage, setActivePage] = useState(HOME)
+  const [measurements, setMeasurements] = useState(sampleMeas)
+
+//TODO: Read the measurements from the API and use subscriptions to remain up to date
+
+  useEffect(() => {
+  },[])
+
 
   const pageContent = () => {
     switch(activePage) {
       case HOME:
         return <HomeContent />
       case VISUALIZATIONS:
-        return <VisualizationsContent />
+        return <VisualizationsContent measurements={measurements} />
       case SETTINGS: 
         return <SettingsContent />
       default: 
--- src/pages/VisualizationsContent.tsx
+++ src/pages/VisualizationsContent.tsx
@@ -1,6 +1,34 @@
 import React from 'react'
+import { VictoryBar, VictoryChart } from 'victory'
 
-const VisualizationsContent = () => {
-    return <div></div>
+type Measurement = {
+  measurementDate: string
+  temperature: number
+  humidity: number
+}
+
+interface VisualizationContentProps {
+  measurements: Array<Measurement>
+}
+
+const timestampToDate = (ts: any) => {
+    if (ts) {
+        // TODO: Check the validity of the TS
+        return new Intl.DateTimeFormat('de-DE', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(ts)
+    }
+    return null
+}
+
+
+const VisualizationsContent = (props: VisualizationContentProps) => {
+    const temperatureMeasurements = props.measurements.map(({measurementDate, temperature}) => ({measurementDate, temperature}))
+    temperatureMeasurements.forEach(element => {
+        console.log(element.measurementDate, timestampToDate(parseInt(element.measurementDate) * 1000))
+    });
+  return (
+    <VictoryChart>
+      <VictoryBar data={temperatureMeasurements} x="measurementDate" y="temperature"/>
+    </VictoryChart>
+  )
 }
 export default VisualizationsContent
