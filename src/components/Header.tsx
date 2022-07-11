d1e91f8b94f709536e84a63e912417a8945ed03e
Feetu Nyrhinen <feetu.nyrhinen@adesso.de>
2022-07-11 13:48:06 +0200

    Do not use mui Box for now, as it causes problems

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,11 @@ import logoImg from '../assets/img/logo_transparent.png'
 
 const Header = (): JSX.Element => {
   return (
-    <Box width={1}> 
-      <img src={logoImg} alt='logo' height={360} width={360} />
-    </Box>
+    <>
+      <div>
+        <img src={logoImg} alt='logo' height={360} width={360} />
+      </div>
+    </>
   )
 }
 export default Header
